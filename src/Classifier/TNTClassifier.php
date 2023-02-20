fcb91c414e0166dc8d4d22bffb89ebfd6c88df8d
Alex Svtwola <alex.cartagena@hotmail.com>
2023-02-20 08:46:40 +0100

    Added probability percentage

--- src/Classifier/TNTClassifier.php
+++ src/Classifier/TNTClassifier.php
@@ -25,9 +25,11 @@ class TNTClassifier
     {
         $words = $this->tokenizer->tokenize($statement);
 
+        $best_likelihoods = [];
         $best_likelihood = -INF;
         $best_type       = '';
         foreach ($this->types as $type) {
+            $best_likelihoods[$type] = -INF;
             $likelihood = log($this->pTotal($type)); // calculate P(Type)
             $p          = 0;
             foreach ($words as $word) {
@@ -37,13 +39,29 @@ class TNTClassifier
             $likelihood += $p; // calculate P(word, Type)
             if ($likelihood > $best_likelihood) {
                 $best_likelihood = $likelihood;
+                $best_likelihoods[$type] = $likelihood;
                 $best_type       = $type;
             }
         }
+
         return [
             'likelihood' => $best_likelihood,
+            'likelihoods' => $best_likelihoods,
+            'probability' => $this->softmax($best_likelihoods),
             'label'      => $best_type
         ];
+    }
+
+    private function softmax(array $v) {
+        //Just in case values are passed in as string, apply floatval
+        $v = array_map('exp',array_map('floatval',$v));
+        $sum = array_sum($v);
+
+        foreach($v as $index => $value) {
+            $v[$index] = $value/$sum;
+        }
+
+        return $v;
     }
 
     public function learn($statement, $type)
